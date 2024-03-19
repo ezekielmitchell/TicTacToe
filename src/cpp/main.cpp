afd78ac0037a4d806a4a8c64b8b7cf5e6c58ac00
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-03-19 16:28:54 -0700

    tictactoe v0.3

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <algorithm>
 
 using namespace std;
 // declare standards
 int userChoice; // user input choice
 int userOptions[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // possible user options
-vector<int> userPicks;
+char xo[2] = {'X', 'O'};
+vector<int> userPicks = {0};
 char userInput;
 
 // declare board
@@ -40,11 +42,12 @@ int main() {
     return 0;
 }
 
-// check user input for validity
+// check user input for validity with location
 bool validOption(int userChoice) {
     for (int number : userOptions) {
         if (number == userChoice) {
             return true;
+            break;
         }
     }
     return false;
@@ -55,6 +58,18 @@ bool openOption(int userChoice) {
     for (int number : userPicks) {
         if (number != userChoice) {
             return true;
+            break;
+        }
+    }
+    return false;
+}
+
+// check user input for validity with input
+bool validInput(int userInput) {
+    for (char c : xo) {
+        if (c == userChoice) {
+            return true;
+            break;
         }
     }
     return false;
@@ -66,24 +81,14 @@ void player1() {
     cout << "(1) Enter the block # you want to edit: ";
     cin >> userChoice;
 
-    if (validOption && openOption) {
+    if (validOption(userChoice) && openOption(userChoice)) {
+        userPicks.push_back(userChoice);
         editBoard(userChoice);
-    } else {
-        cout << "Invalid choice!\nPlease enter a valid remaining option";
     }
 
 }
 
 void player2() {
-    
-    cout << "(2) Enter the block # you want to edit: ";
-    cin >> userChoice;
-
-    if (validOption && openOption) {
-        editBoard(userChoice);
-    } else {
-        cout << "Invalid choice!\nPlease enter a valid remaining option";
-    }
 
 }
 
@@ -91,36 +96,39 @@ void editBoard(int userChoice) {
     cout << "Enter the value you want to input (X/O): ";
     cin >> userInput;
 
-    switch (userChoice) {
-    case 1:
-        board[0][0] = userInput;
-        break;
-    case 2:
-        board[0][2] = userInput;
+    if (validInput(userInput)) {
+        switch (userChoice) {
+            case 1:
+                board[0][0] = userInput;
+                break;
+            case 2:
+                board[0][2] = userInput;
+                    break;
+            case 3:
+                board[0][4] = userInput;
+                break;
+            case 4:
+                board[2][0] = userInput;
+                break;
+            case 5:
+                board[2][2] = userInput;
+                break;
+            case 6:
+                board[2][4] = userInput;
+                break;
+            case 7:
+                board[4][0] = userInput;
+                break;
+            case 8:
+                board[4][2] = userInput;
+                break;
+            case 9:
+                board[4][4] = userInput;
             break;
-    case 3:
-        board[0][4] = userInput;
-        break;
-    case 4:
-        board[2][0] = userInput;
-        break;
-    case 5:
-        board[2][2] = userInput;
-        break;
-    case 6:
-        board[2][4] = userInput;
-        break;
-    case 7:
-        board[4][0] = userInput;
-        break;
-    case 8:
-        board[4][2] = userInput;
-        break;
-    case 9:
-        board[4][4] = userInput;
-       break;
-    default:
-        break;
+            default:
+                break;
+        }
+        
     }
 }
 
